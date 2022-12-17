40b8b5b0b162c955659dfe889325a4ebd791ac16
Till Hemmerich <till.hemmerich@syntax-institut.de>
2022-12-17 16:11:13 +0100

    Initial Commit

---
+++ 2.5LottoSwiftUi/__5LottoSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  __5LottoSwiftUiApp.swift
+//  2.5LottoSwiftUi
+//
+//  Created by Till Hemmerich on 17.12.22.
+//
+
+import SwiftUI
+
+@main
+struct __5LottoSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
